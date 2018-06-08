a8beb78fde447433a1c4f03fd46aeb8f007b7c77
Andres Rios <rianby64@gmail.com>
2018-06-08 22:43:18 +0300

    Add an small refactoring

--- generator.js
+++ generator.js
@@ -1,25 +1,39 @@
 'use strict';
 
+var len = 2;
 function generator(N) {
   let i = 0;
   let json = {};
 
+  let period = 0;
   while (true) {
-    if (JSON.stringify(json).length >= N) break;
-    json[i++] = Math.ceil(Math.random(N) * N);
+    if (len >= N) break;
+    let key = i++;
+    let value = Math.ceil(Math.random(N) * N);
+    json[key] = value;
+    len += 3
+        + key.toString().length
+        + value.toString().length
+        + (period++ > 0 ? 1 : 0);
   }
-
-  if (JSON.stringify(json).length >= N) {
-    delete json[--i];
+  if (len >= N && i > 1) {
+    let key = --i;
+    let value = json[i];
+    delete json[i];
+    len -= 3
+        + key.toString().length
+        + value.toString().length
+        + (period++ > 0 ? 1 : 0);
   }
   return json;
 }
 
 if (process.argv[2]) {
+  var rjson = generator(process.argv[2]);
   var fs = require('fs');
   fs.writeFile(
     'data.json',
-    JSON.stringify(generator(process.argv[2])),
+    JSON.stringify(rjson),
     'utf8', () => {});
 }
 module.exports = generator;
